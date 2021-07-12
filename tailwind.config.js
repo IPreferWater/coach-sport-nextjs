320fd4ddaecdffa450800b4909c488dd6cf6993b
IPreferWater <ipreferwater2@gmail.com>
2021-07-12 14:18:03 +0200

    feat(index2): push new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,13 @@ module.exports = {
       colors: {
         'tana-black': '#161616',
         'tana-red': '#485460',
-        'tana-red-2': '#889eb5',
-        'tana-red-3': '#a8c4df',
-        'tana-red-4': '#99a0a6',
+        'tana-red-2': '#2ecc71',
+        'tana-red-3': '#0C64FA',
+        'tana-red-4': '#16a085',
+        'tana-red-5': '#00B0E3',
+        'tana-red-6': '#f368e0',
+        
+        
       },
       fill: theme => theme('colors'),
       screens:{
